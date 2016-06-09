7bf1aa259272f8066b014125920bb5486a74339b
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-09 16:24:33 +0530

    Add input fields for username and repository with validations

--- app/my_component.js
+++ app/my_component.js
@@ -5,21 +5,42 @@ class MyComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "World"
+      customError: "",
+      username: "",
+      repository: ""
     }
   }
 
-  onChange(e) {
-    this.setState({name: e.target.value})
+  handleClick(e){
+    this.validate(this.state.username, this.state.repository);
+  }
+
+  validate(username, repository){
+    if((!username) || (!repository)){
+      console.log("Inside validate");
+      this.setState({customError: "Please enter a github project to deploy"});
+    }else{
+      this.setState({customError: ""});
+    }
+  }
+
+  userNameChange(e) {
+    this.setState({username: e.target.value});
+  }
+
+  repositoryChange(e){
+    this.setState({repository: e.target.value})
   }
 
   render() {
     return (
-      <div>
-        <div> Hello {this.state.name} </div>
-        <input value={this.state.name} onChange={this.onChange.bind(this)}></input>
-        <div>Change this div without reloading the page and resetting state.</div>
-      </div>
+      <div>https://github.com/
+        <input name="username" onChange={this.userNameChange.bind(this)}/>/
+        <input name="repository" onChange={this.repositoryChange.bind(this)}/>
+        <button name="deploy" onClick={this.handleClick.bind(this)}>Deploy</button>
+        <br />
+        <div>{this.state.customError}</div>
+        </div>
     )
   }
 }
