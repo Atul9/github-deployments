57b3aabb1ef26df1ebfad9435e808c6ff342674b
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-13 16:45:32 +0530

    Refactor DeploymentDisplay as stateless component

--- app/deployment_display.js
+++ app/deployment_display.js
@@ -1,30 +1,28 @@
-import React, {Component} from 'react';
-class DeploymentDisplay extends Component {
-  render() {
-    return (
-      <div>Deployments
-        <table>
-          <thead>
-            <tr>
-              <th> Name</th>
-              <th> Log</th>
-              <th> Url</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.deployments.map((deployment, i) => {
-              return (
-                <tr key={i}>
-                  <td key="a">{deployment.name}</td>
-                  <td key="b">{deployment.logs}</td>
-                  <td key="c">{deployment.url}</td>
-                </tr>
-              )})}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+import React from 'react';
+const DeploymentDisplay = (props) => {
+  return (
+    <div>Deployments
+      <table>
+        <thead>
+          <tr>
+            <th> Name</th>
+            <th> Log</th>
+            <th> Url</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.deployments.map((deployment, i) => {
+            return (
+              <tr key={i}>
+                <td key="a">{deployment.name}</td>
+                <td key="b">{deployment.logs}</td>
+                <td key="c">{deployment.url}</td>
+              </tr>
+            )})}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 DeploymentDisplay.propTypes = { deployments: React.PropTypes.array.isRequired };
