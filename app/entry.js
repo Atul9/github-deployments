2560f70e47ea8a5cd9aebd8a41f8eb62a97b5e9e
Atul Bhosale <atul1bhosale@gmail.com>
2016-06-13 11:59:26 +0530

    Rename MyComponent to DeploymentForm

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyComponent from './my_component';
+import DeploymentForm from './deployment_form';
 
-ReactDOM.render(<MyComponent/>, document.getElementById('root'));
+ReactDOM.render(<DeploymentForm />, document.getElementById('root'));
